7a92ddcf60417f95bc420014c993f408dc0f0e19
王则昆 <364127728@qq.com>
2017-08-28 14:13:37 +0800

    资源初次提交

---
+++ Scripts/Tools/MyResources.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using WZK;
+
+public class MyResources
+{
+    public static T Load<T>(string path) where T : Object
+    {
+        ResourcesScriptableObject so = MemoryPrefs.GetObject<ResourcesScriptableObject>("序列化资源列表");
+        if (so == null)
+        {
+            Debug.Log("不存在该序列化资源列表");
+            return default(T);
+        }
+        path = "Resources/" + path;
+        return (T)so.GetObject(path);
+    }
+}
