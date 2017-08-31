3cd2255820582c832bf200da5aee8c4d305a4ab8
王则昆 <364127728@qq.com>
2017-08-31 13:48:31 +0800

    shaderEditor更新

--- Editor/Tools/ShaderEditor.cs
+++ Editor/Tools/ShaderEditor.cs
@@ -5,37 +5,37 @@ using UnityEngine;
 
 public class ShaderEditor : Editor
 {
-    [MenuItem("Tools/一键将")]
-    static void ShaderSoftEdgeUnlit()
-    {
-        Shader shader = Shader.Find("Unlit/Transparent");
-        string[] tempMaterialsPath = AssetDatabase.GetAllAssetPaths();
-        List<Material> tempMaterials = new List<Material>();
-        for (int i = 0; i < tempMaterialsPath.Length; i++)
-        {
-            string ext = Path.GetExtension(tempMaterialsPath[i]);
-            if (ext != ".mat")
-            {
-                continue;
-            }
-            tempMaterials.Add(AssetDatabase.LoadAssetAtPath(tempMaterialsPath[i], typeof(Material)) as Material);
-        }
+    //[MenuItem("Tools/一键将")]
+    //static void ShaderSoftEdgeUnlit()
+    //{
+    //    Shader shader = Shader.Find("Unlit/Transparent");
+    //    string[] tempMaterialsPath = AssetDatabase.GetAllAssetPaths();
+    //    List<Material> tempMaterials = new List<Material>();
+    //    for (int i = 0; i < tempMaterialsPath.Length; i++)
+    //    {
+    //        string ext = Path.GetExtension(tempMaterialsPath[i]);
+    //        if (ext != ".mat")
+    //        {
+    //            continue;
+    //        }
+    //        tempMaterials.Add(AssetDatabase.LoadAssetAtPath(tempMaterialsPath[i], typeof(Material)) as Material);
+    //    }
         
-        if (tempMaterials.Count != 0)
-        {
-            for (int i = 0; i < tempMaterials.Count; i++)
-            {
-                if (tempMaterials[i] == null)
-                {
-                    continue;
-                }
-                if (tempMaterials[i].shader.name == "Legacy Shaders/Diffuse")
-                {
-                    if(tempMaterials[i].name=="mmdgx_dj_bandeng_yy(chunjie)")
-                    Debug.Log(tempMaterials[i].name);
-                    //tempMaterials[i].shader = shader;
-                }
-            }
-        }
-    }
+    //    if (tempMaterials.Count != 0)
+    //    {
+    //        for (int i = 0; i < tempMaterials.Count; i++)
+    //        {
+    //            if (tempMaterials[i] == null)
+    //            {
+    //                continue;
+    //            }
+    //            if (tempMaterials[i].shader.name == "Legacy Shaders/Diffuse")
+    //            {
+    //                if(tempMaterials[i].name=="mmdgx_dj_bandeng_yy(chunjie)")
+    //                Debug.Log(tempMaterials[i].name);
+    //                //tempMaterials[i].shader = shader;
+    //            }
+    //        }
+    //    }
+    //}
 }
