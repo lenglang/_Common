05809fa152f01ed0a0f84ab3c9da0fb1022312a4
王则昆 <364127728@qq.com>
2017-09-05 18:06:54 +0800

    代码更新

--- Editor/Tools/AssetBundleEditor.cs
+++ Editor/Tools/AssetBundleEditor.cs
@@ -9,14 +9,12 @@ namespace WZK
     /// </summary>
     public class AssetBundleEditor : Editor
     {
-
-        [MenuItem("AssetBundle/打包选中文件为一个AssetBundle(可多选)-压缩")]
-
+        [MenuItem("AssetBundle/分别打包选中文件为AssetBundle-压缩")]
         static void BulidCompressedAssetBundle()
         {
             BuildAssetBundle();
         }
-        [MenuItem("AssetBundle/打包选中文件为一个AssetBundle(可多选)-不压缩")]
+        [MenuItem("AssetBundle/分别打包选中文件为AssetBundle-不压缩")]
         static void BulidUncompressedAssetBundle()
         {
             BuildAssetBundle(false);
@@ -25,23 +23,19 @@ namespace WZK
         {
             if (Selection.objects.Length == 0)
                 return;
-            for (int i = 0; i < Selection.objects.Length; i++)
+            Object[] objects = Selection.objects;
+            for (int i = 0; i < objects.Length; i++)
             {
-                Build(Selection.objects[i], IscompressedAssetBundle);
+                BuildOne(objects[i], IscompressedAssetBundle);
             }
-            Debug.LogError("共打包" + Selection.objects.Length + "个AssetBundle");
         }
-
-
-        static void Build(Object obj, bool IscompressedAssetBundle=true)
+        static void BuildOne(Object obj, bool IscompressedAssetBundle=true)
         {
             AssetBundleBuild assetBundleBuild = new AssetBundleBuild();
-            assetBundleBuild = new AssetBundleBuild();
-            assetBundleBuild.assetNames = new string[Selection.objects.Length];
+            assetBundleBuild.assetNames = new string[1];
             assetBundleBuild.assetNames[0] = AssetDatabase.GetAssetPath(obj);
             string directoryName = Path.GetDirectoryName(assetBundleBuild.assetNames[0]);
             assetBundleBuild.assetBundleName = Path.GetFileName(assetBundleBuild.assetNames[0]) + ".unity3d";
-
 #if UNITY_ANDROID
             directoryName += "/Android";
 #endif
@@ -50,9 +44,53 @@ namespace WZK
 #endif
             if (!Directory.Exists(directoryName))
                 Directory.CreateDirectory(directoryName);
+            Debug.Log(directoryName);
 #if UNITY_ANDROID
             if(IscompressedAssetBundle){BuildPipelineHelper.BuildAssetBundles(directoryName, new AssetBundleBuild[] { assetBundleBuild }, BuildAssetBundleOptions.None, BuildTarget.Android);}
             else{BuildPipelineHelper.BuildAssetBundles(directoryName, new AssetBundleBuild[] { assetBundleBuild }, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.Android);}
+#endif
+
+#if UNITY_IOS
+            if (IscompressedAssetBundle) { BuildPipelineHelper.BuildAssetBundles(directoryName, new AssetBundleBuild[] { assetBundleBuild }, BuildAssetBundleOptions.None, BuildTarget.iOS); }
+            else { BuildPipelineHelper.BuildAssetBundles(directoryName, new AssetBundleBuild[] { assetBundleBuild }, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.iOS); }
+#endif
+        }
+
+        [MenuItem("AssetBundle/打包选中文件为一个AssetBundle-压缩")]
+        static void BulidCompressedAssetBundleAll()
+        {
+            BuildAll();
+        }
+        [MenuItem("AssetBundle/打包选中文件为一个AssetBundle-不压缩")]
+        static void BulidUncompressedAssetBundleAll()
+        {
+            BuildAll(false);
+        }
+        static void BuildAll(bool IscompressedAssetBundle = true)
+        {
+            if (Selection.objects.Length == 0)
+                return;
+            AssetBundleBuild assetBundleBuild = new AssetBundleBuild();
+            assetBundleBuild.assetNames = new string[Selection.objects.Length];
+            for (int i = 0; i < Selection.objects.Length; i++)
+                assetBundleBuild.assetNames[i] = AssetDatabase.GetAssetPath(Selection.objects[i]);
+            string directoryName = Path.GetDirectoryName(assetBundleBuild.assetNames[0]);
+            if (Selection.objects.Length == 1)
+                assetBundleBuild.assetBundleName = Path.GetFileName(assetBundleBuild.assetNames[0]) + ".unity3d";
+            else
+                assetBundleBuild.assetBundleName = Path.GetFileName(directoryName) + ".unity3d";
+#if UNITY_ANDROID
+        directoryName += "/Android";
+#endif
+#if UNITY_IOS
+            directoryName += "/iOS";
+#endif
+            if (!Directory.Exists(directoryName))
+                Directory.CreateDirectory(directoryName);
+
+#if UNITY_ANDROID
+        if(IscompressedAssetBundle){BuildPipelineHelper.BuildAssetBundles(directoryName, new AssetBundleBuild[] { assetBundleBuild }, BuildAssetBundleOptions.None, BuildTarget.Android);}
+         else{BuildPipelineHelper.BuildAssetBundles(directoryName, new AssetBundleBuild[] { assetBundleBuild }, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.Android);}
 #endif
 
 #if UNITY_IOS
--- 没办法/合包/MyResources.cs
+++ 没办法/合包/MyResources.cs
@@ -1,52 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using WZK;
-
-public class MyResources:WZK.Singleton<MyResources>
+namespace WZK
 {
-    public ResourcesScriptableObject _resourcesScriptableObject;
-    public List<Sprite> _spriteList = new List<Sprite>();
-    public List<Texture> _textureList = new List<Texture>();
-    protected override void Awake()
+    public class MyResources : Singleton<MyResources>
     {
-        base.Awake();
-    }
-    public T Load<T>(string path) where T : UnityEngine.Object
-    {
-        Debug.Log("--------" + path);
-        path = "Assets/合包资源/" + path;
-        return (T)_resourcesScriptableObject.GetObject(path);
-    }
-    public Sprite LoadSprite(string path)
-    {
-        Debug.Log("++++++++" + path);
-        string name = path.Substring(path.LastIndexOf("/")+1);
-        Debug.Log(name);
-        return _spriteList.Find(n => n.name == name);
-    }
-    public Texture LoadTexture(string path)
-    {
-        Debug.Log("主题" + path);
-        string name = path.Substring(path.LastIndexOf("/") + 1);
-        return _textureList.Find(n => n.name == name);
-    }
-    public T[] LoadAll<T>(string path) where T : UnityEngine.Object
-    {
-        Debug.Log("图集" + path);
-        List<T> list = new List<T>();
-        for (int i = 0; i < _resourcesScriptableObject._objectList.Count; i++)
+        [Header("ScriptableObject共享资源")]
+        public ResourcesScriptableObject _resourcesScriptableObject;
+        [Header("Sprite集合")]
+        public List<Sprite> _spriteList = new List<Sprite>();
+        [Header("Texture集合")]
+        public List<Texture> _textureList = new List<Texture>();
+        protected override void Awake()
         {
-            if (_resourcesScriptableObject._objectList[i]._assetPath.Contains(path))
+            base.Awake();
+        }
+        public T Load<T>(string path) where T : Object
+        {
+            Debug.Log("合包资源:" + path);
+            path = "Assets/合包资源/" + path;
+            return (T)_resourcesScriptableObject.GetObject(path);
+        }
+        public Sprite LoadSprite(string path)
+        {
+            Debug.Log("Sprite:" + path);
+            string name = path.Substring(path.LastIndexOf("/") + 1);
+            Debug.Log(name);
+            return _spriteList.Find(n => n.name == name);
+        }
+        public Texture LoadTexture(string path)
+        {
+            Debug.Log("Texture:" + path);
+            string name = path.Substring(path.LastIndexOf("/") + 1);
+            return _textureList.Find(n => n.name == name);
+        }
+        public List<T> LoadAll<T>(string path) where T : Object
+        {
+            Debug.Log("LoadAll:" + path);
+            List<T> list = new List<T>();
+            for (int i = 0; i < _resourcesScriptableObject._objectList.Count; i++)
             {
-                list.Add((T)_resourcesScriptableObject._objectList[i]._object);
+                if (_resourcesScriptableObject._objectList[i]._assetPath.Contains(path))
+                {
+                    list.Add((T)_resourcesScriptableObject._objectList[i]._object);
+                }
             }
+            return list;
         }
-        T[] t = new T[list.Count];
-        for (int i = 0; i < list.Count; i++)
-        {
-            t[i] = list[i];
-        }
-        return t;
     }
 }
